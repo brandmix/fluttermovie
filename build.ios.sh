d4d1680a8c7ad9af25b07b6a0ae3c8d2fa33d70d
genx7up <genx7up@users.noreply.github.com>
2020-08-09 04:40:12 +0000

    IOS build

--- build.ios.sh
+++ build.ios.sh
@@ -1,6 +1,20 @@
+#!/usr/bin/env bash
+
 set -xe
 
+FLUTTER_VER=1.20.1
 
-exit
-(cd mobile && flutter build ios)
-(cd mobile/ios && bundle update && bundle exec fastlane beta)
+mkdir -p ~/flutter && cd ~/flutter
+wget https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_${FLUTTER_VER}-stable.zip
+unzip flutter_macos_${FLUTTER_VER}-stable.zip
+
+export PATH="$PATH:`pwd`/flutter/bin"
+flutter precache
+
+cd ${GITHUB_WORKSPACE}/mobile
+flutter doctor
+flutter build ios
+
+cd ios
+bundle update
+bundle exec fastlane beta
