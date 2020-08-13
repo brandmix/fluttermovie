6db71ef1e4975df62cf74ffa311da7bcfd3f7d3c
genx7up <genx7up@users.noreply.github.com>
2020-08-12 22:38:15 -0700

    IOS fixes

---
+++ _ops/publish.ios.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+set -x
+
+if [[ "$OSTYPE" != "darwin"* ]]; then
+	echo "---"
+	echo "Need MacOS"
+	exit 1
+fi
+
+if [[ ! -z "${GITHUB_WORKSPACE:-}" ]]; then
+	pushd ${GITHUB_WORKSPACE:-.}/mobile/ios
+        export TEMP_KEYCHAIN_NAME=fastlane_$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w ${1:-16} | head -n 1)
+	export TEMP_KEYCHAIN_PASSWORD=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w ${1:-64} | head -n 1)
+	bundle exec fastlane beta
+	popd
+fi
