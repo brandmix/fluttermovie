7259ba6a0567e2234b67506b17950511a469143b
genx7up <genx7up@users.noreply.github.com>
2020-08-13 09:25:43 -0700

    IOS fixes

--- _ops/publish.ios.sh
+++ _ops/publish.ios.sh
@@ -12,8 +12,9 @@ fi
 
 if [[ ! -z "${GITHUB_WORKSPACE:-}" ]]; then
 	pushd ${GITHUB_WORKSPACE:-.}/mobile/ios
-        export TEMP_KEYCHAIN_NAME=fastlane_$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w ${1:-16} | head -n 1)
-	export TEMP_KEYCHAIN_PASSWORD=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w ${1:-64} | head -n 1)
+        export TEMP_KEYCHAIN_NAME=fastlane_$(cat /dev/urandom | env LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w ${1:-16} | head -n 1)
+	export TEMP_KEYCHAIN_PASSWORD=$(cat /dev/urandom | env LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w ${1:-64} | head -n 1)
+	echo "here" && sleep 10
 	bundle exec fastlane beta
 	popd
 fi
