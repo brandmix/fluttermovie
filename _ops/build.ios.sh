a7b075d2a2d715433f7087074fad4931eb47c7ec
genx7up <genx7up@users.noreply.github.com>
2020-08-13 10:09:45 -0700

    IOS fixes

--- _ops/build.ios.sh
+++ _ops/build.ios.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 
 set -x
 
-random16=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 16 | head -n 1)
+random16=$(LC_ALL=C; dd if=/dev/urandom bs=256 count=1 2> /dev/null | tr -dc 'a-zA-Z0-9' | head -c 16; echo)
 echo $random16
 exit 1
 
