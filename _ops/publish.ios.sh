b689a231ec7128a123d0986b5723304a39fe8de5
genx7up <genx7up@users.noreply.github.com>
2020-08-13 10:27:05 -0700

    IOS fixes

--- _ops/publish.ios.sh
+++ _ops/publish.ios.sh
@@ -17,10 +17,8 @@ if [[ ! -z "${GITHUB_WORKSPACE:-}" ]]; then
 	bundle update
 	pod install
 
-        export TEMP_KEYCHAIN_NAME=fastlane_$(LC_ALL=C; dd if=/dev/urandom bs=256 count=1 2> /dev/null | tr -dc 'a-zA-Z0-9' | head
-	-c 16; echo)
-	export TEMP_KEYCHAIN_PASSWORD=$(LC_ALL=C; dd if=/dev/urandom bs=256 count=1 2> /dev/null | tr -dc 'a-zA-Z0-9' | head -c
-	64; echo)
+        export TEMP_KEYCHAIN_NAME=fastlane_$(LC_ALL=C; dd if=/dev/urandom bs=256 count=1 2> /dev/null | tr -dc 'a-zA-Z0-9' | head -c 16; echo)
+	export TEMP_KEYCHAIN_PASSWORD=$(LC_ALL=C; dd if=/dev/urandom bs=256 count=1 2> /dev/null | tr -dc 'a-zA-Z0-9' | head -c 64; echo)
 
 	bundle exec fastlane beta
 
